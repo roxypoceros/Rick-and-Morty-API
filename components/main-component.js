8cae585385ef77cd98089d2cfd4e3042abeb886c
roxypoceros <roxypoceros@gmail.com>
2022-12-01 16:47:23 -0600

    Ya se ven las tarjetas con estilo en pantalla

--- components/main-component.js
+++ components/main-component.js
@@ -1,8 +1,7 @@
-import { LitElement, html, css } from 'lit';
-import "./card-component"
+import { LitElement, html, css } from "lit";
+import "./card-component";
 
 export class MainComponent extends LitElement {
-
   static get properties() {
     return {
       characters: { type: Object },
@@ -20,25 +19,64 @@ export class MainComponent extends LitElement {
     this.getCharacters();
   }
 
-  render() {
-    return html`
-      <p>Esta es la API de rick and morty</p>
-      ${console.log(this.characters, "en render")
+  static styles = [
+    css`
+      :host {
+        display: block;
+        --LightGreen: #e6ffe6;
+        --DarkGreen: #006666;
+
       }
 
+      .card {
+        background-color: var(--LightGreen);
+        color: var(--DarkGreen);
+        margin: 16px;
+        padding: 16px;
+        max-width: 500px;
+        border-radius: 25px;
+        text-align: center;
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 
+        0 3px 10px 0 rgba(0, 0, 0, 0.19);
+      }
+      img{
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 
+        0 3px 10px 0 rgba(0, 0, 0, 0.19);
+      }
+    `,
+  ];
+
+  render() {
+    return html`
+      ${console.log(this.character, "en render")}
+
+      <div>
+        ${this.characters.map((e) => html`
+            <div class="container">
+              <div class="card">
+                <h2>#${e.id}</h2>
+
+                <h2>Name:${e.name}</h2>
+                <img src=${e.image} />
+                <p>Status:${e.status}</p>
+                <p>Species:${e.species}</p>
+                <p>Gender:${e.gender}</p>
+                <p>Origin:${e.origin.name}</p>
+              </div>
+            </div>
+          `
+        )}
+      </div>
     `;
   }
 
-  getCharacters(){
-       fetch("https://rickandmortyapi.com/api/character")
-      .then(response => response.json()) //convertir resultado a JSON
-      .then(json => 
-        this.characters = json.results) // imprimir resultado en consola
-        console.log(this.characters)
-      //.catch(err => console.log("Error", err))
+  getCharacters() {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json()) //convertir resultado a JSON
+      .then((json) => (this.characters = json.results)); // imprimir resultado en consola
+    console.log(this.characters);
+    //.catch(err => console.log("Error", err))
   }
 }
 
-
-
-customElements.define('main-component', MainComponent);
+customElements.define("main-component", MainComponent);
